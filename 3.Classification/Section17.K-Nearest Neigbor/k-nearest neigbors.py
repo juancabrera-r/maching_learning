7cecc11f7251575782a146bb099055750a251edb
Juan Manuel <juanmanuelcabrera.r@gmail.com>
2022-04-24 18:13:08 +0100

    K-NN STEPS

---
+++ 3.Classification/Section17.K-Nearest Neigbor/k-nearest neigbors.py
@@ -0,0 +1,8 @@
+'''
+K-NN
+    1. Choose the number K of neighbors
+    2. Take the K nearest neighbors of the new data point, according to the Euclidean distance (distance bw 2 points)
+    3. Among these k neighbors, count the number of data points in each category
+    4. Assign the new data point to th ecategory where you counted the most neighbors
+'''
+
