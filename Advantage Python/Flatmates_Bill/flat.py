f85c2dfa1adfc7919f6be5a611869a5ee021d26d
Juan Manuel <juanmanuelcabrera.r@gmail.com>
2022-04-24 16:43:11 +0100

    Refactor Class PDFGenerator

--- Advantage Python/Flatmates_Bill/flat.py
+++ Advantage Python/Flatmates_Bill/flat.py
@@ -1,7 +1,5 @@
 import webbrowser
 
-from fpdf import FPDF
-
 
 class Bill:
     '''
@@ -28,44 +26,3 @@ class Flatmate:
         return to_pay
 
 
-class PDFReport:
-    '''
-    Generate a PDF withe the name of the flatemates, the period and how much have to pay
-    '''
-
-    def __init__(self, filename):
-        self.filename = filename
-
-    def generate(self, flatmate1, flatmate2, bill):
-
-        flatmate1_pay = str(round(flatmate1.pays(bill, flatmate2), 2))
-        flatmate2_pay = str(round(flatmate2.pays(bill, flatmate1), 2))
-
-        pdf = FPDF(orientation='P', unit='pt', format='A4')
-        pdf.add_page()
-
-        #Add icon
-        pdf.image(name='house.png', w=30, h=30)
-
-        #insert title
-        pdf.set_font(family='Times', size=24, style='B')
-        pdf.cell(w=0, h=80, txt='Flatmate Bill', border=0, align='C', ln=1)
-
-        #insert period label and value
-        pdf.set_font(family='Times', size=14, style='B')
-        pdf.cell(w=100, h=40, txt='Period', border=0)
-        pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
-
-        #insert name and due amount of the first flatmate
-        pdf.set_font(family='Times', size=12)
-        pdf.cell(w=100, h=25, txt=flatmate1.name, border=0)
-        pdf.cell(w=100, h=25, txt=flatmate1_pay, border=0, ln=1)
-
-        #insert name and due amount of the first flatmate
-        pdf.cell(w=100, h=25, txt=flatmate2.name, border=0)
-        pdf.cell(w=100, h=25, txt=flatmate2_pay, border=0)
-
-        pdf.output(self.filename)
-
-        #Open the pdf automatically
-        webbrowser.open(self.filename)
