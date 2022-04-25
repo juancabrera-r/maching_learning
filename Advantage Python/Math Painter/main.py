284623553f85677d8c126d7eddb10d4c3de566f1
Juan Manuel <juanmanuelcabrera.r@gmail.com>
2022-04-25 19:19:53 +0100

    Created rectangle and square class

--- Advantage Python/Math Painter/main.py
+++ Advantage Python/Math Painter/main.py
@@ -9,10 +9,13 @@ class Canvas:
         self.width = width
         self.color = color
 
+        #Create a 3d numpy array of zeros
+        self.data = np.zeros((self.width, self.height, 3), dtype=np.uint8)
+        #Giving a color
+        self.data[:] = self.color
+
     def make(self, canvas_name):
-        canvas = np.zeros((self.width, self.height, 3), dtype=np.uint8)
-        canvas[:] = self.color
-        img = Image.fromarray(canvas, 'RGB')
+        img = Image.fromarray(self.data, 'RGB')
         img.save(canvas_name)
 
 class Square:
@@ -23,8 +26,8 @@ class Square:
         self.X = X
         self.Y = Y
 
-    def make(self, canvas):
-        square = np.zeros((self.side, self.side, 3), dtype=np.uint8)
+    def draw(self, canvas):
+        canvas.data[self.X: self.X + self.side, self.Y: self.Y + self.side] = self.color
 
 
 class Rectangle:
@@ -36,9 +39,13 @@ class Rectangle:
         self.Y = Y
         self.X = X
 
-    def make(self, canvas):
-        pass
+    def draw(self, canvas):
+        canvas.data[self.X : self.X + self.width , self.Y : self.Y + self.height] = self.color
 
 
-canvas = Canvas(200, 200, (255,255,255))
+canvas = Canvas(20, 30, (255,255,255))
+r1 = Rectangle(1,6, 10, 12, (234,252,0))
+r1.draw(canvas)
+s1 = Square(5,12, 3, (0,252,244))
+s1.draw(canvas)
 canvas.make('canvas.png')
