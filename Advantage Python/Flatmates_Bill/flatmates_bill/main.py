4805947311878cd5976418f8d1327fda94127c86
Juan Manuel <juanmanuelcabrera.r@gmail.com>
2022-05-07 07:20:52 +0100

    Create main.py (using Flask) and refactor (main.py, flat.py & report.py)

---
+++ Advantage Python/Flatmates_Bill/flatmates_bill/main.py
@@ -0,0 +1,21 @@
+from flat import Bill, Flatmate
+from report import PDFReport
+
+amount = float(input('Hey user, enter the bill amount: '))
+period = input('What is the period?: ')
+
+name1 = input('What is the name of one flatmate?: ')
+days_in_house = int(input(f'How many days did {name1} stay in the house during the bill perios? '))
+
+name2 = input('What is the name of the other flatmate?: ')
+days_in_house2 = int(input(f'How many days did {name2} stay in the house during the bill perios? '))
+
+a_bill = Bill(amount=amount, period=period)
+mate1 = Flatmate(name=name1, days_in_house=days_in_house)
+mate2 = Flatmate(name=name2, days_in_house=days_in_house2)
+
+print(f'{name1} pays: ', mate1.pays(bill=a_bill, flatmate2=mate2))
+print('Marry pays: ', mate2.pays(a_bill, mate1))
+
+pdf_report = PDFReport(filename=f'{a_bill.period}.pdf')
+pdf_report.generate(flatmate1=mate1, flatmate2=mate2, bill=a_bill)
--- Advantage Python/Flatmates_Bill/main.py
+++ Advantage Python/Flatmates_Bill/main.py
@@ -1,21 +1,28 @@
-from flat import Bill, Flatmate
-from report import PDFReport
+from flask.views import MethodView
+from wtforms import Form
+from flask import Flask
 
-amount = float(input('Hey user, enter the bill amount: '))
-period = input('What is the period?: ')
+app = Flask(__name__)
 
-name1 = input('What is the name of one flatmate?: ')
-days_in_house = int(input(f'How many days did {name1} stay in the house during the bill perios? '))
+class HomePage(MethodView):
 
-name2 = input('What is the name of the other flatmate?: ')
-days_in_house2 = int(input(f'How many days did {name2} stay in the house during the bill perios? '))
+    def get(self):
+        return 'Hello'
 
-a_bill = Bill(amount=amount, period=period)
-mate1 = Flatmate(name=name1, days_in_house=days_in_house)
-mate2 = Flatmate(name=name2, days_in_house=days_in_house2)
+class BillFormPage(MethodView):
 
-print(f'{name1} pays: ', mate1.pays(bill=a_bill, flatmate2=mate2))
-print('Marry pays: ', mate2.pays(a_bill, mate1))
+    def get(self):
+        return 'Im the bill form page'
 
-pdf_report = PDFReport(filename=f'{a_bill.period}.pdf')
-pdf_report.generate(flatmate1=mate1, flatmate2=mate2, bill=a_bill)
+class ResultsPage(MethodView):
+    pass
+
+
+class BillForm(Form):
+    pass
+
+
+app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
+app.add_url_rule('/bill', view_func=BillFormPage.as_view('form_page'))
+
+app.run()
