869ed65f62913a45d1851996aa321164263c0eb8
Juan Manuel <juanmanuelcabrera.r@gmail.com>
2022-05-07 07:34:49 +0100

    Create templates (index & bill_form_page) and modify main.py

--- Advantage Python/Flatmates_Bill/main.py
+++ Advantage Python/Flatmates_Bill/main.py
@@ -1,18 +1,23 @@
 from flask.views import MethodView
 from wtforms import Form
-from flask import Flask
+from flask import Flask, render_template
+
+'''
+IMPORTANT!!!!
+The folder templates, can't have other name!. It should be named 'templates'
+'''
 
 app = Flask(__name__)
 
 class HomePage(MethodView):
 
     def get(self):
-        return 'Hello'
+        return render_template('index.html')
 
 class BillFormPage(MethodView):
 
     def get(self):
-        return 'Im the bill form page'
+        return render_template('bill_form_page.html')
 
 class ResultsPage(MethodView):
     pass
@@ -23,6 +28,6 @@ class BillForm(Form):
 
 
 app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
-app.add_url_rule('/bill', view_func=BillFormPage.as_view('form_page'))
+app.add_url_rule('/bill', view_func=BillFormPage.as_view('bill_form_page'))
 
 app.run()
